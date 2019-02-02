6053c3bb0b135c8e627f664a5b9cef0b01dc0d97
KimSoungRyoul <KimSoungRyoul@gmail.com>
2019-02-02 17:58:31 +0900

    hello py unitTest

---
+++ tests/test_mocking_requests.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestExternalAPI(unittest.TestCase):
+
+    def test_asdf(self):
+        a = 1
+        b = 2
+        self.assertEqual(a + b, 3)
+
+    def test_asdsdfsdff(self):
+        a = 1
+        b = 2
+        self.assertEqual(a + b, 3)
